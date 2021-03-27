bed3a9cfbcc39c95ecd14f5a5c26b54a66aca04d
b21627428 <b21627428@cs.hacettepe.edu.tr>
2021-03-28 00:20:25 +0300

    first commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# ExpressJS-Rest Api
+# Web3-Ethereum Provider
+# Truffle-Smart Contract Migration
+# Solidity-Smart Contract
+
+# Javascript-Smart Contract Testing and Rest Api Testing
---
+++ Truffle/contracts/Sample.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.16;
+
+contract Sample{
+    int sum;
+    function Sum(int _a,int _b) public{
+
+    }
+    function getSum() public view returns(int){
+        return sum;
+    }
+}
