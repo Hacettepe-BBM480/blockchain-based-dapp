6fff82776f09db02f9f46576b0c928885e14d379
b21627428 <b21627428@cs.hacettepe.edu.tr>
2021-04-02 05:18:12 +0300

    deneme commit

--- Truffle/contracts/Sample.sol
+++ Truffle/contracts/Sample.sol
@@ -1,9 +1,10 @@
-pragma solidity ^0.5.16;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.3;
 
 contract Sample{
     int sum;
     function Sum(int _a,int _b) public{
-
+        sum = _a + _b;
     }
     function getSum() public view returns(int){
         return sum;
